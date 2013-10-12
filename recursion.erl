6ed052d57290c879392cecc32c8d489be42cb7b9
Mathieu Lajugie <mathieul@gmail.com>
2013-10-11 22:37:05 -0700

    add zip

--- recursion.erl
+++ recursion.erl
@@ -17,3 +17,14 @@ reverse(List) -> reverse(List, []).
 reverse([], Acc) -> Acc;
 reverse([Head | Rest], Acc) ->
   reverse(Rest, [Head | Acc]).
+
+take(List, Number) -> take(List, Number, []).
+take(_, 0, Acc) -> reverse(Acc);
+take([Head | Rest], Number, Acc) when Number > 0 ->
+  take(Rest, Number - 1, [Head | Acc]).
+
+zip(List1, List2) -> zip(List1, List2, []).
+zip([], _, Acc) -> lists:reverse(Acc);
+zip(_, [], Acc) -> lists:reverse(Acc);
+zip([Head1 | Rest1], [Head2 | Rest2], Acc) ->
+  zip(Rest1, Rest2, [{Head1, Head2} | Acc]).
